1440b99bdf5cc237ed970edc4b23b0a60e744d41
H1rono <hronok66@gmail.com>
2022-08-30 17:56:07 +0900

    現在のメインプログラム #2

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,52 @@
 #include <Arduino.h>
+#include <Servo.h>
 #include <ssr/PS4.hpp>
 #include <ssr/PS4Controller.hpp>
+#include <ps4operation/Log.hpp>
+#include <ps4operation/SingleMotor.hpp>
+#include <ps4operation/Solenoid.hpp>
+#include <ps4operation/LowerBody.hpp>
+#include <ps4operation/Servo.hpp>
 
-class PS4LogOperation : public ssr::PS4Operation {
-public:
-    void operate(const ssr::PS4Value & value) override {
-        char log[256] = "";
-        sprintf(
-            log,
-            "(%3d %3d) (%3d %3d) [%d %d %d %d] [%d %d %d %d] [%d %d %d] [%d %3d %d] [%d %3d %d]\n",
-            value.lstick.x, value.lstick.y, value.rstick.x, value.rstick.y,
-            value.up, value.right, value.down, value.left,
-            value.triangle, value.circle, value.cross, value.square,
-            value.ps, value.share, value.options,
-            value.l1, value.l2, value.l3,
-            value.r1, value.r2, value.r3
-        );
-        Serial.print(log);
-    }
-};
-
-ssr::PS4Controller_Bluetooth ps4Controller(true);
+Servo servo;
+//ssr::PS4Controller_Bluetooth ps4Controller(true);
+ssr::PS4Controller_USB ps4Controller{};
+ps4operation::SingleMotor * motor = nullptr;
+ps4operation::Solenoid * solenoid = nullptr;
+ps4operation::LowerBody * lowerBody = nullptr;
 
 void setup() {
     Serial.begin(115200);
     if (ps4Controller.begin() == -1) {
-        Serial.println("failed to initialize");
+        Serial.println(F("failed to initialize"));
         while (1);
     }
-    ps4Controller.addOperation(new PS4LogOperation());
-    Serial.println("start");
+    /* log */
+    ps4Controller.addOperation(new ps4operation::Log());
+    /* motor */
+    motor = new ps4operation::SingleMotor(3, 4);
+    ps4Controller.addOperation(motor);
+    motor->begin();
+    /* solenoid */
+    solenoid = new ps4operation::Solenoid(7, 100);
+    solenoid->begin();
+    ps4Controller.addOperation(solenoid);
+    /* lowerbody */
+    lowerBody = new ps4operation::LowerBody(6, 7, 4, 5, 2, 3);
+    lowerBody->begin();
+    ps4Controller.addOperation(lowerBody);
+    /* servo */
+    ps4operation::Servo * servoOperation = new ps4operation::Servo(servo);
+    ps4Controller.addOperation(servoOperation);
+    servoOperation->attach(23);
+    Serial.println(F("start"));
 }
 
 void loop() {
-    if (!ps4Controller.connected()) {
-        Serial.println("not connected");
-    }
+    /* solenoid */
+    solenoid->update();
     ps4Controller.update();
-    // delay(100);
+    if (!ps4Controller.connected()) {
+        Serial.println(F("not connected"));
+    }
 }
