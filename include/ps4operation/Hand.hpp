5c7fbbc64744de72e86d99f0f29de7d02efd8467
H1rono <hronok66@gmail.com>
2022-09-06 19:26:51 +0900

    Handクラスを追加

---
+++ include/ps4operation/Hand.hpp
@@ -0,0 +1,70 @@
+/**
+ * @file Hand.hpp
+ * @author H1rono (hronok66@gmail.com)
+ * @brief 把持機構を扱うOperation
+ * @copyright Copyright (c) 2022 ssr2022-saka-maza
+ */
+
+#pragma once
+
+#ifndef PS4_OPERATION_HAND_HPP
+
+/**
+ * @brief ps4operation/Hand.hppがインクルードされていることを示すdefine
+ */
+#define PS4_OPERATION_HAND_HPP
+
+#include <Arduino.h>
+#include <ssr/SyncServo.hpp>
+#include <ssr/PS4Operation.hpp>
+
+/**
+ * @brief ssr::PS4Operationを継承したクラス群
+ */
+namespace ps4operation {
+    class Hand : public ssr::PS4Operation {
+    private:
+        /**
+         * @brief 制御する同期サーボ
+         */
+        ssr::SyncServo & _syncServo;
+
+        const uint8_t _minAngle;
+
+        const uint8_t _maxAngle;
+
+        /**
+         * @brief 現在の角度
+         */
+        float _angle;
+
+    public:
+        /**
+         * @brief Construct a new Hand object
+         * @param syncServo ssr::SyncServo & 制御する同期サーボ
+         * @param minAngle uint8_t とれる角度の最小値
+         * @param maxAngle uint8_t とれる角度の最大値
+         */
+        explicit Hand(ssr::SyncServo & syncServo, uint8_t minAngle = 60, uint8_t maxAngle = 120);
+
+        // デフォルトコンストラクタ、コピーを禁止
+        Hand() = delete;
+        Hand(const Hand &) = delete;
+        Hand & operator=(const Hand &) = delete;
+
+        /**
+         * @brief 初期化処理 全体のsetup()内で呼び出すこと
+         * @param angle 最初の角度
+         */
+        void begin(uint8_t angle = 90);
+
+        /**
+         * @brief コントローラーのセンサー値でハンドを制御
+         * @param value コントローラーのセンサー値
+         * @details L1ボタンで開く、R2ボタンで閉じる
+         */
+        virtual void operate(const ssr::PS4Value & value) override;
+    };
+} // namespace ps4operation
+
+#endif /* PS4_OPERATION_HAND_HPP */
---
+++ src/ps4operation/Hand.cpp
@@ -0,0 +1,23 @@
+#include "ps4operation/Hand.hpp"
+
+ps4operation::Hand::Hand(ssr::SyncServo & syncServo, uint8_t minAngle, uint8_t maxAngle)
+: _syncServo(syncServo), _minAngle(minAngle), _maxAngle(maxAngle), _angle(0) {}
+
+void ps4operation::Hand::begin(uint8_t angle) {
+    _angle = max(_maxAngle, min(_minAngle, uint8_t(angle)));
+    _syncServo.mirrored = true;
+    _syncServo.write(uint8_t(_angle));
+}
+
+void ps4operation::Hand::operate(const ssr::PS4Value & value) {
+    _angle += (value.l1 - value.r1) / float(_maxAngle - _minAngle);
+    _angle = max(_maxAngle, min(_minAngle, _angle));
+    _syncServo.write(uint8_t(_angle));
+    #ifdef ps4operation_verbose
+    char buffer[256] = "";
+    char * ptr = buffer;
+    ptr += snprintf_P(ptr, 200, PSTR("[ps4operation::Hand] current angle is "));
+    ptr += dtostrf(_angle, 6, 2, ptr);
+    Serial.println(buffer);
+    #endif /* ps4operation_verbose */
+}
