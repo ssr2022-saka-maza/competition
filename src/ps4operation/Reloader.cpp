0c9ff14381bc001f0109f9bb7e1508870dfb91ec
H1rono <hronok66@gmail.com>
2022-09-09 01:56:15 +0900

    装填機構の動作確認 #8

--- src/ps4operation/Reloader.cpp
+++ src/ps4operation/Reloader.cpp
@@ -12,8 +12,8 @@ void ps4operation::Reloader::begin(uint8_t angle) {
 void ps4operation::Reloader::operate(const ssr::PS4Value & value) {
     int8_t d = value.triangle - value.cross;
     if (d != 0) _dAngle = d;
-    _angle += _dAngle / float(_maxAngle - _minAngle);
-    _angle = max(_maxAngle, min(_minAngle, _angle));
+    _angle += _dAngle / 3.0;
+    _angle = max(_minAngle, min(_maxAngle, _angle));
     _syncServo.write(uint8_t(_angle));
     #ifdef ps4operation_verbose
     char buffer[256] = "";
