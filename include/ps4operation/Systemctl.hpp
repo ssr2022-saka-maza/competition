8e6dceefd0e52e5af9578f9ecfc7bdb270f4b705
H1rono <hronok66@gmail.com>
2022-09-13 15:46:27 +0900

    Systemctlクラスを実装

--- include/ps4operation/Systemctl.hpp
+++ include/ps4operation/Systemctl.hpp
@@ -15,6 +15,8 @@
  */
 #define PS4_OPERATION_SYSTEMCTL_HPP
 
+#include <Arduino.h>
+#include <ssr/PS4Operation.hpp>
 #include <ps4operation/Resetable.hpp>
 
 /**
@@ -23,8 +25,36 @@
 namespace ps4operation {
     class Systemctl : public ps4operation::Resetable {
     private:
+        enum class State {
+            start,
+            stop,
+            automate,
+        };
+
+        uint8_t _childCount;
+
+        Resetable ** _children;
+
+        State _state;
+
+        ssr::PS4Value _lastValue;
+
+        uint64_t _stateUpdateTime;
 
     public:
+        const uint8_t childCapacity;
+
+        Systemctl(uint8_t childCapacity);
+
+        ~Systemctl();
+
+        void reset() noexcept override;
+
+        void addChild(Resetable * child) noexcept;
+
+        void removeChild(Resetable * child) noexcept;
+
+        virtual void operate(const ssr::PS4Value & value) noexcept override;
 
     }; // class Systemctl
 } // namespace ps4operation
---
+++ src/ps4operation/Systemctl.cpp
@@ -0,0 +1,120 @@
+#include "ps4operation/Systemctl.hpp"
+
+ps4operation::Systemctl::Systemctl(uint8_t childCapacity)
+: _childCount(0), _children(nullptr), _state(State::start), _lastValue(), _stateUpdateTime(0), childCapacity(childCapacity) {
+    _children = new Resetable*[this->childCapacity];
+    for (uint8_t i = 0; i < this->childCapacity; ++i) {
+        _children[i] = nullptr;
+    }
+}
+
+ps4operation::Systemctl::~Systemctl() {
+    delete[] _children;
+}
+
+void ps4operation::Systemctl::reset() noexcept {
+    for (uint8_t i = 0; i < _childCount; ++i) {
+        _children[i]->reset();
+    }
+}
+
+void ps4operation::Systemctl::addChild(Resetable * child) noexcept {
+    if (_childCount < childCapacity) {
+        _children[_childCount] = child;
+        ++_childCount;
+    }
+}
+
+void ps4operation::Systemctl::removeChild(Resetable * child) noexcept {
+    for (uint8_t i = 0; i < _childCount; ++i) {
+        if (_children[i] == child) {
+            _children[i] = _children[_childCount - 1];
+            _children[_childCount - 1] = nullptr;
+            --_childCount;
+            break;
+        }
+    }
+}
+
+void ps4operation::Systemctl::operate(const ssr::PS4Value & value) noexcept {
+    bool square = value.square;
+    bool ps = value.ps;
+    bool otherwise = _lastValue != value;
+    State newState = _state;
+    switch (_state) {
+        case State::start:
+            if (ps) {
+                newState = State::stop;
+                _lastValue = value;
+            } else if (square) {
+                newState = State::automate;
+                _lastValue = value;
+            } else if (otherwise) {
+                newState = State::start;
+            }
+            break;
+        case State::stop:
+            if (ps) {
+                newState = State::stop;
+                _lastValue = value;
+            } else if (square) {
+                newState = State::automate;
+                _lastValue = value;
+            } else if (otherwise) {
+                newState = State::start;
+            }
+            break;
+        case State::automate:
+            if (ps) {
+                newState = State::stop;
+                _lastValue = value;
+            } else if (square) {
+                newState = State::automate;
+                _lastValue = value;
+            } else if (otherwise) {
+                newState = State::start;
+            }
+            break;
+    }
+    if (newState != _state) {
+        _stateUpdateTime = millis();
+        _state = newState;
+    }
+    switch (_state) {
+        case State::start:
+            for (uint8_t i = 0; i < _childCount; ++i) {
+                _children[i]->operate(value);
+            }
+            break;
+        case State::stop:
+            if (millis() - _stateUpdateTime > 1000) {
+                reset();
+            }
+            break;
+        case State::automate:
+            uint64_t dt = (millis() - _stateUpdateTime) / 1000;
+            ssr::PS4Value newValue = value;
+            newValue.square = false;
+            newValue.ps = false;
+            /*
+            0- 3: ハンドを閉じる
+            3- 6: ハンドを回転
+            6- 9: ハンドを開く
+            9-12: ハンドを閉じながらハンドを回転
+            */
+            if (dt < 3) {
+                newValue.r1 = true;
+            } else if (dt < 6) {
+                newValue.triangle = true;
+            } else if (dt < 9) {
+                newValue.l1 = true;
+            } else if (dt < 12) {
+                newValue.r1 = true;
+                newValue.cross = true;
+            }
+            for (uint8_t i = 0; i < _childCount; ++i) {
+                _children[i]->operate(newValue);
+            }
+            break;
+    }
+}
